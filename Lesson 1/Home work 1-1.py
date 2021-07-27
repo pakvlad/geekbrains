eabc5301720f6b6cc1b4a8dfd2269ae81047f512
Vladimir <pakvova94@mail.ru>
2021-07-27 17:02:43 +0500

    Первая выгрузка домашнего задания

---
+++ Lesson 1/Home work 1-1.py
@@ -0,0 +1,9 @@
+# игра с переменными
+n = 'Cristiano Ronaldo: 7'
+s = 'Lionel Messi: 10'
+print(n, 'и', s)
+# работа с переменными
+name = input("Как Вас зовут?")
+print('Привет', name)
+age = input("сколько Вам лет?")
+print(name,'тебе',age,'лет')
