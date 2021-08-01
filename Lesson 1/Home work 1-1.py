68d578d8ef6155ccc2320b36c885810f584b0884
pakvlad <87746572+pakvlad@users.noreply.github.com>
2021-08-01 11:45:24 +0500

    Delete Lesson 1 directory

--- Lesson 1/Home work 1-1.py
+++
@@ -1,9 +0,0 @@
-# игра с переменными
-n = 'Cristiano Ronaldo: 7'
-s = 'Lionel Messi: 10'
-print(n, 'и', s)
-# работа с переменными
-name = input("Как Вас зовут?")
-print('Привет', name)
-age = input("сколько Вам лет?")
-print(name,'тебе',age,'лет')
