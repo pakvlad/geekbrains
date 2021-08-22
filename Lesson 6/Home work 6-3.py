854f96bc39ba5bb8af60d0b1e54f7331bac767e8
Vladimir <pakvova94@mail.ru>
2021-08-22 16:26:02 +0500

    Загрузка ДЗ 6 урока

---
+++ Lesson 6/Home work 6-3.py
@@ -0,0 +1,23 @@
+class Worker:
+
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": wage, "bonus": bonus}
+
+class Position(Worker):
+
+    def __init__(self, name, surname, position, wage, bonus):
+        super().__init__(name, surname, position, wage, bonus)
+
+    def get_full_name(self):
+        return self.name + ' ' + self.surname
+
+    def get_total_income(self):
+        return self._income.get('wage') + self._income.get('bonus')
+
+full = Position('Султан', 'Султангазин', 'Охранник', 200000, 50000)
+print(full.get_full_name())
+print(full.position)
+print(full.get_total_income())
